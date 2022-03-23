aa5fd221c8391d52f7f089d7c9a483ada442cc57
James McNeil <mcneil_james@hotmail.com>
2022-03-23 19:45:19 +0000

    added dummy test data

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,9 +1,33 @@
 import ExpenseItem from './components/ExpenseItem'
 
 const App = () => {
+  const expenses = [
+    {
+      id: 1,
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2022, 2, 28),
+    },
+    {
+      id: 2,
+      title: 'Car Fuel',
+      amount: 150,
+      date: new Date(2022, 4, 2),
+    },
+    {
+      id: 3,
+      title: 'Food Shopping',
+      amount: 330,
+      date: new Date(2022, 0, 15),
+    },
+  ]
+
   return (
     <div>
-      <ExpenseItem />
+      {expenses.length > 0 &&
+        expenses.map((expense) => (
+          <ExpenseItem key={expense.id} expense={expense} />
+        ))}
     </div>
   )
 }
