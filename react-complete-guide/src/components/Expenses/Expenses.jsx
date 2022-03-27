1c2ae6b263bf67be2f27996c6e09da47f0bbe153
James McNeil <mcneil_james@hotmail.com>
2022-03-27 20:24:17 +0100

    created onSelect Year function

--- react-complete-guide/src/components/Expenses/Expenses.jsx
+++ react-complete-guide/src/components/Expenses/Expenses.jsx
@@ -1,10 +1,15 @@
 import Card from '../UI/Card'
+import ExpensesFilter from './EpensesFilter'
 import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
 
-const Expenses = ({ expenses }) => {
+const Expenses = ({ expenses, filterExpenses }) => {
+  const onSelectYear = (year) => {
+    filterExpenses(year)
+  }
   return (
     <Card className='expenses'>
+      <ExpensesFilter onSelectYear={onSelectYear} />
       {expenses.length > 0 &&
         expenses.map((expense) => (
           <ExpenseItem key={expense.id} expense={expense} />
