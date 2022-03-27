7a2999145dc7ab1d13c2d0eb973a8972584394b9
James McNeil <mcneil_james@hotmail.com>
2022-03-27 18:25:30 +0100

    created onSaveExpnse as prop to get expenseData

--- react-complete-guide/src/components/NewExpense/NewExpense.jsx
+++ react-complete-guide/src/components/NewExpense/NewExpense.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 import ExpenseForm from './ExpenseForm'
 import './NewExpense.css'
 
-const NewExpense = () => {
+const NewExpense = ({ addExpenseHandler }) => {
+  const onSaveExpense = (expense) => {
+    const expenseData = {
+      ...expense,
+      date: new Date(expense.date),
+      id: Math.random().toString(),
+    }
+    // console.log(expenseData)
+    addExpenseHandler(expenseData)
+  }
   return (
     <div className='new-expense'>
-      <ExpenseForm />
+      <ExpenseForm onSaveExpense={onSaveExpense} />
     </div>
   )
 }
