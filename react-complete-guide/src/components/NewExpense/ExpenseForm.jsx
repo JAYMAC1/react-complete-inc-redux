c71c058783264464ff3b3e104bbe3d807eaf694f
James McNeil <mcneil_james@hotmail.com>
2022-03-27 18:28:54 +0100

    used onSaveExpense prop to return formData

--- react-complete-guide/src/components/NewExpense/ExpenseForm.jsx
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './ExpenseForm.css'
-const ExpenseForm = () => {
+const ExpenseForm = ({ onSaveExpense }) => {
   // const [enteredTitle, setEnteredTitle] = useState('')
   // const [enteredAmount, setEnteredAmount] = useState('')
   // const [enteredDate, setEnteredDate] = useState('')
@@ -50,7 +50,7 @@ const ExpenseForm = () => {
 
   const formSubmitHandler = (e) => {
     e.preventDefault()
-    console.log(formData)
+    onSaveExpense(formData)
     setFormData({
       title: '',
       amount: '',
