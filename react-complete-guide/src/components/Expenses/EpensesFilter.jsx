1e23f2e2db2a1db475744fcd4320c4f152319708
James McNeil <mcneil_james@hotmail.com>
2022-03-28 09:47:57 +0100

    aded ALL as option

--- react-complete-guide/src/components/Expenses/EpensesFilter.jsx
+++ react-complete-guide/src/components/Expenses/EpensesFilter.jsx
@@ -11,6 +11,7 @@ const ExpensesFilter = ({ onSelectYear }) => {
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
         <select onChange={handleChange}>
+          <option value='ALL'>All</option>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
